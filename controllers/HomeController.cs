b74295321d8c2b990a2a006b23544c99500eed77
musma <musma@DESKTOP-HLTTOQN>
2022-11-29 23:56:28 +0500

    Add project files.

---
+++ controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WAD_SRMS_Project.controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Adminlogin()
+        {
+            return View();
+        }
+        public IActionResult signup()
+        {
+            return View();
+        }
+        public IActionResult Addmission()
+        {
+            return View();
+        }
+    }
+}
